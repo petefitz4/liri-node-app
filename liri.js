702a38248bd72fdc52c47b2aa8e3e1fcefa448dc
petefitz4 <petefitz4@gmail.com>
2017-11-15 17:15:34 -0600

    coded functions

--- liri.js
+++ liri.js
@@ -6,69 +6,122 @@ var request = require("request");
 var fs = require("fs");
 
 //get data from keys.js and store in variable
-var datakey = require("./keys.js");
-
-/*
-//https://www.npmjs.com/package/twitter 
-var client = new Twitter({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '',
-  access_token_secret: ''
-});
- 
-var params = {screen_name: 'nodejs'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
-  }
-});
-*/
-
-//https://www.npmjs.com/package/node-spotify-api
-
-//login: use Facebook credentials
-//app name: Concert Reviewers
-//Client ID: 651cfff19a3a4307a895e9dcf1d18d9a
-//Client Secret: 9a0170325c2f4a3389bbb59ec76941fd
-
-/*
-var Spotify = require('node-spotify-api');
- 
-var spotify = new Spotify({
-  id: <your spotify client id>,
-  secret: <your spotify client secret>
-});
- 
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
- 
-console.log(data); 
-});
-*/
+var dataKey = require("./keys.js");
 
 //take in following commands
 //`my-tweets`
 //`spotify-this-song`
 //`movie-this`
 //`do-what-it-says`
+var commands = process.argv[2];
+//build array for multiple word entry search
+var search = "";
+
+function commandInput(commands, search){
+	search = getMoreWords();
+
+	switch (commands) {
+		//get list of tweets
+		case "my-tweets":myTweets();
+		break;
+		
+		//get song info
+		case "spotify-this-song":
+		//associate search string to variable based on command name
+		var songName = search;
+		//if no song entered then default to The Sign
+		if (songName === "") {
+			searchDefaultSong();
+		}
+		else {
+			searchSongInfo(songName);
+		}
+		break;
+		
+		//get movie info
+		//associate search string to variable based on command name
+		case "movie-this": searchMovieInfo(search);break;
+		
+		//var movieName = search;
+		//if no movie name entered then default to Mr Nobody
+		//if (movieName === ""){
+		//searchMovieInfo("Mr. Nobody");}
+		//else {searchMovieInfo(movieName);}
+
+		//get info from text file
+		case "do-what-it-says": doWhatItSays();
+		break;
+	}
+}
+
+//for loop to obtain additional words in string search
+function getMoreWords(){
+	searchArray = process.argv;
+
+	for (var i =3; i <searchArray.length;i++){
+		search += searchArray[i];
+	}
+	return search;
+}
 
 
-//1. command to show last 20 tweets by typing node liri.js my-tweets
+//1. function to show last 20 tweets by typing node liri.js my-tweets
+//https://www.npmjs.com/package/twitter 
+function myTweets(){
+	//grab twitter keys from keys.js
+	var client = new Twitter(dataKey.twitterKeys);
+	//search parameter for last 20 tweets with my username
+	var params = {q: '@BeersLax', count: 20};
+	//search function syntax
+	client.get('search/tweets', params, function(error, tweets, response) {
+		if(!error){
+			for (var i=0; i < params.count; i++){
+				console.log(tweets);
+				logOutput(tweets);
+			}
+		}
+		else {
+			console.log(error);
+			logOutput(error);
+		}
+   });
+}
 
-
-//2. command to show following info - node liri.js spotify-this-song '<song name here>'
-//Artist
-//The song's name
-//A preview link of the song from Spotify
-//The album that the song is from
-
-
+//2. function to show following info - node liri.js spotify-this-song '<song name here>'
+	//Artist
+	//The song's name
+	//A preview link of the song from Spotify
+	//The album that the song is from
 //If no song then default to "The Sign" by Ace of Base
+	//login: use Facebook credentials
+	//app name: Concert Reviewers
+	//Client ID: 651cfff19a3a4307a895e9dcf1d18d9a
+	//Client Secret: 9a0170325c2f4a3389bbb59ec76941fd
 
-//3. code for movie search node liri.js movie-this '<movie name here>'
+//https://www.npmjs.com/package/node-spotify-api
+function searchSongInfo(songName){
+	if (songName === "") {
+		songName = "The Sign";
+	};
+
+	spotify.search({ type: 'track', query: songName }, function(err, data) {
+  		if (!err) {
+ 			for (var i = 0; i < 10; i++) {
+ 				if (data.tracks.items[i] != undefined){
+ 					console.log("Artist: " + data.tracks.items[i].artists[0].name);
+ 					console.log("Song: "+ data.tracks.items[i].name);
+ 					console.log("Preview Link: " + data.tracks.items[i].preview_url);
+ 					console.log("Album: " + data.tracks.items[i].album.name);
+ 				};			  			
+  			};
+		}
+		else {
+			console.log("Error occurred: " + err);
+		};
+	});
+};
+
+//3. function for movie search node liri.js movie-this '<movie name here>'
 // use OMDB API - http://www.omdbapi.com   key: 40e9cece
 //	"Title"   * Title of the movie.  
 //  "Year"   * Year the movie came out.
@@ -78,44 +131,78 @@ console.log(data);
 //     * Language of the movie.
 //  plot   * Plot of the movie.
 //     * Actors in the movie.
-var nodeArgs = process.argv;
-var movieName = "";
 
-for (var i = 2; i < nodeArgs.length; i++) {
-  if (i > 2 && i < nodeArgs.length) {
-    movieName = movieName + "+" + nodeArgs[i];
-  }
-  else {
-    movieName += nodeArgs[i];
-  }
+function searchMovieInfo(movieName){
+	var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&tomatoes=true&r=json&apikey=40e9cece"
+//OMDB body
+	if(!movieName){
+		movieName = "Mr. Nobody"
+		request(queryUrl, function (error,response, body){
+			// If the request is successful
+			if(!error && response.statusCode==200){
+				// Parse the body of the site
+				var details = JSON.parse(body)
+				console.log("If you haven't watched 'Mr. Nobody then you should: " + "\n")
+				console.log("Title: " + info.Title);
+				console.log("Year: " + info.Year);
+				console.log("IMDB Rating: " + info.imdbRating);
+				console.log("Rotten Tomatoes Rating: " + info.tomatoUserMeter);
+				console.log("Country: " + info.Country + "\n");
+				console.log("Language: "+ info.Language + "\n");
+				console.log("Plot: " + info.Plot + "\n");	
+				console.log("Actors: " + info.Actors + "\n");
+				console.log("It's on Netflix!")
+			}
+			else {
+				console.log("Error occurred" + error);
+			}
+		});
+	}
+	else {
+		request(queryUrl, function (error,response, body){
+			// If the request is successful
+			if(!error && response.statusCode==200){
+				// Parse the body of the site
+				var details = JSON.parse(body)
+				console.log("Title: " + info.Title);
+				console.log("Year: " + info.Year);
+				console.log("IMDB Rating: " + info.imdbRating);
+				console.log("Rotten Tomatoes Rating: " + info.tomatoUserMeter);
+				console.log("Country: " + info.Country + "\n");
+				console.log("Language: "+ info.Language + "\n");
+				console.log("Plot: " + info.Plot + "\n");	
+				console.log("Actors: " + info.Actors + "\n");
+			}
+			else {
+				console.log("Error occurred" + error);
+			}
+		});
+
+	}
+};
+
+
+//4. function to retrieve text from random.txt file when node liri.js do-what-it-says is entered
+function doWhatItSays(){
+	fs.readFile("random.txt", "utf8", function(error, data){
+		if(!error){
+			doWhatItSaysEntry = data.split(",");
+			searchThisSong(doWhatItSaysEntry[0], doWhatItSaysEntry[1]);
+		}
+		else {
+			console.log("Error occurred" + error);
+		}
+	});
+};
+
+//function to add log entries to the log.txt file
+function logOutput(logData){
+	fs.appendFile("log.txt", logData, (error) => {
+		if(error){
+			throw error;
+		}
+	});
 }
-
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece"
-console.log(queryUrl);
-
-request(queryUrl, function(error, response, body) {
-  // If the request is successful
-  if (!error && response.statusCode === 200) {
-    // Parse the body of the site and recover just the imdbRating
-    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-    console.log("Release Year: " + JSON.parse(body).Year);
-  }
-});
-
-/*
-{"Title":"Star Wars: Episode IV - A New Hope","Year":"1977","Rated":"PG","Released":"25 May 1977","Runtime":"121 min","Genre":"Action, Adventure, Fantasy","Director":"George Lucas","Writer":"George Lucas","Actors":"Mark Hamill, Harrison Ford, Carrie Fisher, Peter Cushing","Plot":"Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee, and two droids to save the galaxy from the Empire's world-destroying battle-station, while also attempting to rescue Princess Leia from the evil Darth Vader.","Language":"English","Country":"USA","Awards":"Won 6 Oscars. Another 50 wins & 28 nominations.","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BYTUwNTdiMzMtNThmNS00ODUzLThlMDMtMTM5Y2JkNWJjOGQ2XkEyXkFqcGdeQXVyNzQ1ODk3MTQ@._V1_SX300.jpg","Ratings":[{"Source":"Internet Movie Database","Value":"8.7/10"},{"Source":"Rotten Tomatoes","Value":"93%"},{"Source":"Metacritic","Value":"92/100"}],"Metascore":"92","imdbRating":"8.7","imdbVotes":"1,004,425","imdbID":"tt0076759","Type":"movie","DVD":"21 Sep 2004","BoxOffice":"N/A","Production":"20th Century Fox","Website":"http://www.starwars.com/episode-iv/","Response":"True"}
-
-*/
-
-
-
-//if user doesn't type in a movie else insert 'Mr. Nobody'
-//console.log "If you haven't watched 'Mr. Nobody then you should: "
-//href http://www.imdb.com/title/tt0485947/
-//It's on Netflix
-
-//4. code to retrieve text from random.txt file when node liri.js do-what-it-says is entered
-//use fs node package
 
 
 
